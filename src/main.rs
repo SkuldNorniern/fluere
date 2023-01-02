035f01f406a31416e68b08e8160a905d11276a8d
Skuld Norniern <skuldnorniern@gmail.com>
2023-01-03 02:29:28 +0900

    [ADD] Added tempoary pcap-rs with netflow

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,84 @@
 pub mod net;
 
-use clap::{command, Arg, ArgAction};
+use clap::{arg,Arg,ArgAction,ArgMatches,Command};
 use std::process::exit;
 
-#[tokio::main] 
-async fn main() {
-    let _args = command!()
+fn cli() -> Command{
+    Command::new("fluere")
         .version("1.0")
         .author("Skuld Norniern. <skuldnorniern@gmail.com>")
         .about("Netflow Capture Tool")
-        .arg(
-            Arg::new("list")
-                //.about("List of network interfaces")
-                .short('l')
-                .long("list")
-                .action(ArgAction::SetTrue),
+        .subcommand_required(true)
+        .subcommand(
+             Command::new("online")
+                .about("Capture netflow online")
+                .arg(
+                    Arg::new("interface")
+                        //.about("Select network interface to use")
+                        .short('i')
+                        .long("interface")
+                )
+                .arg(
+                    Arg::new("list")
+                        //.about("List of network interfaces")
+                        .short('l')
+                        .long("list")
+                        .action(ArgAction::SetTrue),
+                )
+            
         )
-        .arg(
-            Arg::new("interface")
-                //.about("Select network interface to use")
-                .short('i')
-                .long("interface")
-                .action(ArgAction::Append),
-        )
-        .get_matches();
-    
+        .subcommand(
+            Command::new("offline")
+                .about("convet pcap files to netflow")
+                .arg(
+                    Arg::new("file")
+                        //.about("name of the pcap file")
+                        .short('f')
+                        .long("file")
+                )
+                .arg(
+                    Arg::new("csv")
+                        //.about("name of the exported csv file")
+                        .short('c')
+                        .long("csv")
+                )
+            )
+}
+
+#[tokio::main]
+async fn main() {
+    let args = cli().get_matches();
+    let _interfaces = net::list_interfaces();
+    let mut interface = "None";
+
+    match args.subcommand(){
+        Some(("online", args)) => {
+            println!("Online mode");
+            if args.contains_id("interface"){
+                println!("Interface {} selected", args.get_one::<String>("interface").unwrap());
+                interface = args.get_one::<String>("interface").unwrap()
+            }
+            if args.contains_id("list"){
+                for (i, interface) in _interfaces.iter().enumerate() {
+                    println!("[{}]: {}",i ,interface.name);
+                }
+                //println!("List of interfaces {:?}", _interfaces);
+                exit(0);
+            }
+               //net::netflow(_interface);
+        },
+        Some(("offline", args)) => {
+            println!("Offline mode");
+            let _file = args.get_one::<String>("file").unwrap();
+            let _csv = args.get_one::<String>("csv").unwrap();
+            //net::netflow(_file, _csv);
+        },
+        _ => {
+            println!("No mode selected");
+            exit(0);
+        }
+    }
+    /*
     let _interfaces = net::list_interfaces();
     let mut interface = "None";
 
@@ -48,6 +102,7 @@ async fn main() {
         println!("No interface selected");
         exit(0);
     }
+    */
     /*if interface.is_none(){
         let mut flag = 0;
         for iface in _interfaces {
