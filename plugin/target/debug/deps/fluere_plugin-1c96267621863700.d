5ad9912d6d2e87ebed97ebe9ae56808250c12dea
SkuldNorniern <skuldnorniern@gmail.com>
2023-02-05 19:06:29 +0900

    [MODIFY] Fixed Cargo.toml

--- plugin/target/debug/deps/fluere_plugin-1c96267621863700.d
+++
@@ -1,5 +0,0 @@
-/Users/skuld/Repository/fluere/plugin/target/debug/deps/fluere_plugin-1c96267621863700: src/main.rs
-
-/Users/skuld/Repository/fluere/plugin/target/debug/deps/fluere_plugin-1c96267621863700.d: src/main.rs
-
-src/main.rs:
