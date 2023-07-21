cdc77dd4fd69917a3029506b2e318f35079361bf
SkuldNorniern <skuldnorniern@gmail.com>
2023-07-21 13:10:02 +0900

    feat!: deprecated TCP NS flag caused by RFC 3540 moved to Historic status

--- src/net/parser/flags.rs
+++ src/net/parser/flags.rs
@@ -39,10 +39,7 @@ pub fn parse_flags(protocol: u8, payload: &[u8]) -> [u8; 9] {
                     0 => 0,
                     _ => 1,
                 },
-                match tcp_flags & 0x100 {
-                    0 => 0,
-                    _ => 1,
-                },
+                0, // Deprecated NS Flag due to Experimental RFC 3530 moved to Historic status
             ]
         }
         _ => [0; 9],
