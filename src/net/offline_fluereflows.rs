c9bcdc1f31cc50561bc73bb757f591be76554bb2
SkuldNorniern <skuldnorniern@gmail.com>
2023-08-18 12:24:23 +0900

    refactor: move to modulated flow update on `offline` mode #20

--- src/net/offline_fluereflows.rs
+++ src/net/offline_fluereflows.rs
@@ -4,14 +4,21 @@ use fluereflow::FluereRecord;
 use pcap::Capture;
 use tokio::task;
 
-use crate::net::parser::{parse_fluereflow, parse_keys, parse_microseconds};
-use crate::net::types::{Key, TcpFlags};
-use crate::types::Args;
-use crate::utils::{cur_time_file, fluere_exporter};
+use crate::{
+    net::{
+        parser::{parse_fluereflow, parse_keys, parse_microseconds}, 
+        flows::update_flow,
+        types::{Key, TcpFlags},
+    },
+    types::{Args,UDFlowKey},
+    utils::{cur_time_file, fluere_exporter},
+};
 
-use std::collections::HashMap;
-use std::fs;
-use std::time::Instant;
+use std::{
+    collections::HashMap,
+    fs,
+    time::Instant,
+};
 
 pub async fn fluereflow_fileparse(arg: Args) {
     let csv_file = arg.files.csv.unwrap();
@@ -90,78 +97,29 @@ pub async fn fluereflow_fileparse(arg: Args) {
         let ttl = flowdata.get_min_ttl();
         //println!("active flows: {:?}", active_flow.len());
         //println!("current inputed flow{:?}", active_flow.get(&key_value).unwrap());
-        if is_reverse {
-            let flow = active_flow.get_mut(&reverse_key).unwrap();
-
-            flow.set_d_pkts(flow.get_d_pkts() + 1);
-            flow.set_in_pkts(flow.get_in_pkts() + 1);
-            flow.set_in_bytes(flow.get_in_bytes() + doctets);
-            flow.set_d_octets(flow.get_d_octets() + doctets);
-            flow.set_max_pkt(flow.get_max_pkt().max(pkt));
-            flow.set_min_pkt(flow.get_min_pkt().min(pkt));
-            flow.set_max_ttl(flow.get_max_ttl().max(ttl));
-            flow.set_min_ttl(flow.get_min_ttl().min(ttl));
-            flow.set_fin_cnt(flow.get_fin_cnt() + flags.fin as u32);
-            flow.set_syn_cnt(flow.get_syn_cnt() + flags.syn as u32);
-            flow.set_rst_cnt(flow.get_rst_cnt() + flags.rst as u32);
-            flow.set_psh_cnt(flow.get_psh_cnt() + flags.psh as u32);
-            flow.set_ack_cnt(flow.get_ack_cnt() + flags.ack as u32);
-            flow.set_urg_cnt(flow.get_urg_cnt() + flags.urg as u32);
-            flow.set_ece_cnt(flow.get_ece_cnt() + flags.ece as u32);
-            flow.set_cwr_cnt(flow.get_cwr_cnt() + flags.cwr as u32);
-            flow.set_ns_cnt(flow.get_ns_cnt() + flags.ns as u32);
-            flow.set_last(time);
+        let flow_key = if is_reverse { &reverse_key } else { &key_value };
+        if let Some(flow) = active_flow.get_mut(flow_key) {
+            let update_key = UDFlowKey {
+                doctets,
+                pkt,
+                ttl,
+                flags,
+                time,
+            };
+            update_flow(flow, is_reverse, update_key);
 
             if verbose >= 2 {
-                println!("reverse flow updated");
+                println!("{} flow updated", if is_reverse { "reverse" } else { "forward" });
             }
 
             if flags.fin == 1 || flags.rst == 1 {
                 if verbose >= 2 {
                     println!("flow finished");
                 }
-                records.push(*active_flow.get(&reverse_key).unwrap());
-                active_flow.remove(&reverse_key);
-            }
-        } else {
-            let flow = active_flow.get_mut(&key_value).unwrap();
-
-            flow.set_d_pkts(flow.get_d_pkts() + 1);
-            flow.set_out_pkts(flow.get_out_pkts() + 1);
-            flow.set_out_bytes(flow.get_out_bytes() + doctets);
-            flow.set_d_octets(flow.get_d_octets() + doctets);
-            flow.set_max_pkt(flow.get_max_pkt().max(pkt));
-            flow.set_min_pkt(flow.get_min_pkt().min(pkt));
-            flow.set_max_ttl(flow.get_max_ttl().max(ttl));
-            flow.set_min_ttl(flow.get_min_ttl().min(ttl));
-            flow.set_fin_cnt(flow.get_fin_cnt() + flags.fin as u32);
-            flow.set_syn_cnt(flow.get_syn_cnt() + flags.syn as u32);
-            flow.set_rst_cnt(flow.get_rst_cnt() + flags.rst as u32);
-            flow.set_psh_cnt(flow.get_psh_cnt() + flags.psh as u32);
-            flow.set_ack_cnt(flow.get_ack_cnt() + flags.ack as u32);
-            flow.set_urg_cnt(flow.get_urg_cnt() + flags.urg as u32);
-            flow.set_ece_cnt(flow.get_ece_cnt() + flags.ece as u32);
-            flow.set_cwr_cnt(flow.get_cwr_cnt() + flags.cwr as u32);
-            flow.set_ns_cnt(flow.get_ns_cnt() + flags.ns as u32);
-            flow.set_last(time);
-
-            if flags.fin == 1 || flags.rst == 1 {
-                if verbose >= 2 {
-                    println!("flow finished");
-                }
-                records.push(*active_flow.get(&key_value).unwrap());
-                active_flow.remove(&key_value);
-            }
-        }
-        /*for (key, flow) in active_flow.clone().iter(){
-            //let flow = active_flow.get(&key).unwrap();
-            if flow.get_last() < (time- (flow_timeout* 1000))
-            {
-                //println!("flow expired");
                 records.push(*flow);
-                active_flow.remove(key);
-            }
-        }*/
+                active_flow.remove(flow_key);
+            }   
+        }
     }
     if verbose >= 1 {
         println!("Captured in {:?}", start.elapsed());
