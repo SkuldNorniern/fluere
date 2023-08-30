021e988338211c354064113343fad94ab08a1e19
SkuldNorniern <skuldnorniern@gmail.com>
2023-08-30 09:03:17 +0900

    feat: async plugin calls #24

--- src/net/online_fluereflow.rs
+++ src/net/online_fluereflow.rs
@@ -55,7 +55,7 @@ pub async fn packet_capture(arg: Args) {
         .unwrap()
         .promisc(true)
         //.buffer_size(100000000)
-        //.immediate_mode(true)
+        .immediate_mode(true)
         .open()
         .unwrap();
 
@@ -80,161 +80,171 @@ pub async fn packet_capture(arg: Args) {
     let mut active_flow: HashMap<Key, FluereRecord> = HashMap::new();
     let mut packet_count = 0;
 
-    while let Ok(packet) = cap.next_packet() {
-        if verbose >= 3 {
-            println!("received packet");
-        }
-
-        let (mut key_value, mut reverse_key) = match parse_keys(packet.clone()) {
-            Ok(keys) => keys,
-            Err(_) => continue,
-        };
-        if !use_mac {
-            key_value.mac_defaultate();
-            reverse_key.mac_defaultate();
-        }
-
-        let (doctets, raw_flags, flowdata) = match parse_fluereflow(packet.clone()) {
-            Ok(result) => result,
-            Err(_) => continue,
-        };
-
-        let flags = TcpFlags::new(raw_flags);
-        //pushing packet in to active_flows if it is not present
-        let is_reverse = match active_flow.get(&key_value) {
-            None => match active_flow.get(&reverse_key) {
-                None => {
-                    // if the protocol is TCP, check if is a syn packet
-                    if flowdata.prot == 6 {
-                        if flags.syn > 0 {
-                            active_flow.insert(key_value, flowdata);
-                            if verbose >= 2 {
-                                println!("flow established");
-                            }
-                        } else {
-                            continue;
-                        }
-                    } else {
-                        active_flow.insert(key_value, flowdata);
-                        if verbose >= 2 {
-                            println!("flow established");
-                        }
-                    }
-
-                    false
-                }
-                Some(_) => true,
-            },
-            Some(_) => false,
-        };
-        /*let is_reverse = if active_flow.contains_key(&key_value) {
-            false
-        } else if active_flow.contains_key(&reverse_key) {
-            true
-        } else {
-            if flowdata.get_prot() != 6  && flags.syn > 0  {
-                active_flow.insert(key_value, flowdata);
-                if verbose >= 2 {
-                    println!("flow established");
-                }
-            } else {
+    loop {
+        match cap.next_packet() {
+            Err(_) => {
                 continue;
             }
-            false
-        };*/
+            Ok(packet) => {
+                if verbose >= 3 {
+                    println!("received packet");
+                }
 
-        let time = parse_microseconds(
-            packet.header.ts.tv_sec as u64,
-            packet.header.ts.tv_usec as u64,
-        );
-        //println!("time: {:?}", time);
-        let pkt = flowdata.min_pkt;
-        let ttl = flowdata.min_ttl;
-        //println!("current inputed flow{:?}", active_flow.get(&key_value).unwrap());
-        let flow_key = if is_reverse { &reverse_key } else { &key_value };
-        if let Some(flow) = active_flow.get_mut(flow_key) {
-            let update_key = UDFlowKey {
-                doctets,
-                pkt,
-                ttl,
-                flags,
-                time,
-            };
-            update_flow(flow, is_reverse, update_key);
+                let (mut key_value, mut reverse_key) = match parse_keys(packet.clone()) {
+                    Ok(keys) => keys,
+                    Err(_) => continue,
+                };
+                if !use_mac {
+                    key_value.mac_defaultate();
+                    reverse_key.mac_defaultate();
+                }
 
-            if verbose >= 2 {
-                println!(
-                    "{} flow updated",
-                    if is_reverse { "reverse" } else { "forward" }
-                );
-            }
+                let (doctets, raw_flags, flowdata) = match parse_fluereflow(packet.clone()) {
+                    Ok(result) => result,
+                    Err(_) => continue,
+                };
 
-            if flags.fin == 1 || flags.rst == 1 {
+                let flags = TcpFlags::new(raw_flags);
+                //pushing packet in to active_flows if it is not present
+                let is_reverse = match active_flow.get(&key_value) {
+                    None => match active_flow.get(&reverse_key) {
+                        None => {
+                            // if the protocol is TCP, check if is a syn packet
+                            if flowdata.prot == 6 {
+                                if flags.syn > 0 {
+                                    active_flow.insert(key_value, flowdata);
+                                    if verbose >= 2 {
+                                        println!("flow established");
+                                    }
+                                } else {
+                                    continue;
+                                }
+                            } else {
+                                active_flow.insert(key_value, flowdata);
+                                if verbose >= 2 {
+                                    println!("flow established");
+                                }
+                            }
+
+                            false
+                        }
+                        Some(_) => true,
+                    },
+                    Some(_) => false,
+                };
+                /*let is_reverse = if active_flow.contains_key(&key_value) {
+                false
+                } else if active_flow.contains_key(&reverse_key) {
+                true
+                } else {
+                if flowdata.get_prot() != 6  && flags.syn > 0  {
+                active_flow.insert(key_value, flowdata);
                 if verbose >= 2 {
-                    println!("flow finished");
+                println!("flow established");
                 }
-                // plugin_manager.process_flow_data(flow).expect("Failed to process flow data");
-                plugin_manager.process_flow_data(*flow).await.unwrap();
+                } else {
+                continue;
+                }
+                false
+                };*/
 
-                records.push(*flow);
+                let time = parse_microseconds(
+                    packet.header.ts.tv_sec as u64,
+                    packet.header.ts.tv_usec as u64,
+                );
+                //println!("time: {:?}", time);
+                let pkt = flowdata.min_pkt;
+                let ttl = flowdata.min_ttl;
+                //println!("current inputed flow{:?}", active_flow.get(&key_value).unwrap());
+                let flow_key = if is_reverse { &reverse_key } else { &key_value };
+                if let Some(flow) = active_flow.get_mut(flow_key) {
+                    let update_key = UDFlowKey {
+                        doctets,
+                        pkt,
+                        ttl,
+                        flags,
+                        time,
+                    };
+                    update_flow(flow, is_reverse, update_key);
 
-                active_flow.remove(flow_key);
-            }
-        }
-
-        packet_count += 1;
-        // slow down the loop for windows to avoid random shutdown
-        if packet_count % sleep_windows == 0 && cfg!(target_os = "windows") {
-            if verbose >= 3 {
-                println!("Slow down the loop for windows");
-            }
-            sleep(Duration::from_millis(0)).await;
-        }
-
-        // Export flows if the interval has been reached
-        if last_export.elapsed() >= Duration::from_millis(interval) && interval != 0 {
-            let mut expired_flows = vec![];
-            packet_count = 0;
-            for (key, flow) in active_flow.iter() {
-                if flow_timeout > 0 && flow.last < (time - (flow_timeout * 1000)) {
                     if verbose >= 2 {
-                        println!("flow expired");
+                        println!(
+                            "{} flow updated",
+                            if is_reverse { "reverse" } else { "forward" }
+                        );
                     }
-                    plugin_manager.process_flow_data(*flow).await.unwrap();
-                    records.push(*flow);
-                    expired_flows.push(*key);
+
+                    if flags.fin == 1 || flags.rst == 1 {
+                        if verbose >= 2 {
+                            println!("flow finished");
+                        }
+                        // plugin_manager.process_flow_data(flow).expect("Failed to process flow data");
+                        plugin_manager.process_flow_data(*flow).await.unwrap();
+
+                        records.push(*flow);
+
+                        active_flow.remove(flow_key);
+                    }
+                }
+
+                packet_count += 1;
+                // slow down the loop for windows to avoid random shutdown
+                if packet_count % sleep_windows == 0 && cfg!(target_os = "windows") {
+                    if verbose >= 3 {
+                        println!("Slow down the loop for windows");
+                    }
+                    sleep(Duration::from_millis(0)).await;
+                }
+
+                // Export flows if the interval has been reached
+                if last_export.elapsed() >= Duration::from_millis(interval) && interval != 0 {
+                    let mut expired_flows = vec![];
+                    packet_count = 0;
+                    for (key, flow) in active_flow.iter() {
+                        if flow_timeout > 0 && flow.last < (time - (flow_timeout * 1000)) {
+                            if verbose >= 2 {
+                                println!("flow expired");
+                            }
+                            plugin_manager.process_flow_data(*flow).await.unwrap();
+                            records.push(*flow);
+                            expired_flows.push(*key);
+                        }
+                    }
+                    active_flow.retain(|key, _| !expired_flows.contains(key));
+                    records.clear();
+                    let cloned_records = records.clone();
+                    let file_path_clone = file_path.clone();
+                    let tasks = task::spawn(async {
+                        let file = fs::File::create(file_path_clone).unwrap();
+                        fluere_exporter(cloned_records, file).await;
+                    });
+
+
+                    let result = tasks.await;
+                    if verbose >= 1 {
+                        println!("Export {} result: {:?}", file_path, result);
+                    }
+                    file_path = cur_time_file(csv_file.as_str(), file_dir, ".csv").await;
+                    file = fs::File::create(file_path.clone()).unwrap();
+                    last_export = Instant::now();
+                }
+
+                // Check if the duration has been reached
+                if start.elapsed() >= Duration::from_millis(duration) && duration != 0 {
+                    let mut expired_flows = vec![];
+                    for (key, flow) in active_flow.iter() {
+                        if flow.last < (time - (flow_timeout * 1000)) {
+                            if verbose >= 2 {
+                                println!("flow expired");
+                            }
+                            records.push(*flow);
+                            expired_flows.push(*key);
+                        }
+                    }
+                    active_flow.retain(|key, _| !expired_flows.contains(key));
+                    break;
                 }
             }
-            active_flow.retain(|key, _| !expired_flows.contains(key));
-            let cloned_records = records.clone();
-            records.clear();
-            let tasks = task::spawn(async {
-                fluere_exporter(cloned_records, file).await;
-            });
-
-            let result = tasks.await;
-            if verbose >= 1 {
-                println!("Export {} result: {:?}", file_path, result);
-            }
-            file_path = cur_time_file(csv_file.as_str(), file_dir, ".csv").await;
-            file = fs::File::create(file_path.clone()).unwrap();
-            last_export = Instant::now();
-        }
-
-        // Check if the duration has been reached
-        if start.elapsed() >= Duration::from_millis(duration) && duration != 0 {
-            let mut expired_flows = vec![];
-            for (key, flow) in active_flow.iter() {
-                if flow.last < (time - (flow_timeout * 1000)) {
-                    if verbose >= 2 {
-                        println!("flow expired");
-                    }
-                    records.push(*flow);
-                    expired_flows.push(*key);
-                }
-            }
-            active_flow.retain(|key, _| !expired_flows.contains(key));
-            break;
         }
     }
     if verbose >= 1 {
@@ -244,13 +254,18 @@ pub async fn packet_capture(arg: Args) {
         plugin_manager.process_flow_data(*flow).await.unwrap();
         records.push(*flow);
     }
+    let cloned_records = records.clone();
+    let file_path_clone = file_path.clone();
     let tasks = task::spawn(async {
-        fluere_exporter(records, file).await;
+        let file = fs::File::create(file_path_clone).unwrap();
+        fluere_exporter(cloned_records, file).await;
     });
+
     plugin_manager.await_completion().await;
     let result = tasks.await;
     if verbose >= 1 {
         println!("Exporting task excutation result: {:?}", result);
     }
     //println!("records {:?}", records);
+
 }
