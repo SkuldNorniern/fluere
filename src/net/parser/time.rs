54c10145073a63c149160fc991e74a53aaf053cc
SkuldNorniern <skuldnorniern@gmail.com>
2023-09-05 14:37:01 +0900

    fix: remove use of deprecated function `from_utc`

--- src/net/parser/time.rs
+++ src/net/parser/time.rs
@@ -1,12 +1,13 @@
 use chrono::prelude::*;
 
+
 pub fn parse_microseconds(sec: u64, usec: u64) -> u64 {
     sec * 1000000 + usec
 }
 
 pub fn microseconds_to_timestamp(usec: u64) -> String {
     let naive = NaiveDateTime::from_timestamp_opt(usec as i64, 0);
-    let datetime = DateTime::<Utc>::from_utc(naive.unwrap(), Utc);
+    let datetime = DateTime::<Utc>::from_naive_utc_and_offset(naive.unwrap(), Utc);
     let newdatetime = datetime.format("%Y-%m-%d_%H-%M-%S UTC").to_string();
     newdatetime
 }
