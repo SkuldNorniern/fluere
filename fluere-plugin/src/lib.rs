804aed0167875153e6c1763592d45cfe35653682
SkuldNorniern <skuldnorniern@gmail.com>
2023-09-06 15:49:56 +0900

    fix: fix error caused by reformat

--- fluere-plugin/src/lib.rs
+++ fluere-plugin/src/lib.rs
@@ -25,59 +25,7 @@ impl PluginManager {
         let lua = Arc::new(Mutex::new(Lua::new()));
         let (sender, receiver) = mpsc::channel::<FluereRecord>(100); // 100 is the channel capacity
         let plugins = Arc::new(Mutex::new(HashSet::new()));
-        //let _ = Self::load_plugins config).await;
-
-        //let worker = Self::start_worker(lua.clone(), receiver, plugins.clone()); /*
-                                                                                   let worker = Arc::new(Mutex::new(tokio::spawn(async move {
-                                                                                   while let Some(data) = receiver.recv().await {
-                                                                                   let lua_guard = lua_clone.lock().await;
-                                                                                   let plugins = plugins_clone.lock().await;
-
-                                                                                   let _ = lua_guard.context(|ctx| -> rlua::Result<()> {
-                                                                                   // Convert the FluereRecord to a Vec<String>
-                                                                                   let record_vec = data.to_vec();
-
-                                                                                   // Convert the Vec<String> to a Lua table
-                                                                                   let lua_table = ctx.create_table()?;
-                                                                                   lua_table.set("source", record_vec[0].clone())?;
-                                                                                   lua_table.set("destination", record_vec[1].clone())?;
-                                                                                   lua_table.set("d_pkts", record_vec[2].clone())?;
-                                                                                   lua_table.set("d_octets", record_vec[3].clone())?;
-                                                                                   lua_table.set("first", record_vec[4].clone())?;
-                                                                                   lua_table.set("last", record_vec[5].clone())?;
-                                                                                   lua_table.set("src_port", record_vec[6].clone())?;
-                                                                                   lua_table.set("dst_port", record_vec[7].clone())?;
-                                                                                   lua_table.set("min_pkt", record_vec[8].clone())?;
-                                                                                   lua_table.set("max_pkt", record_vec[9].clone())?;
-                                                                                   lua_table.set("min_ttl", record_vec[10].clone())?;
-                                                                                   lua_table.set("max_ttl", record_vec[11].clone())?;
-                                                                                   lua_table.set("in_pkts", record_vec[12].clone())?;
-                                                                                   lua_table.set("out_pkts", record_vec[13].clone())?;
-                                                                                   lua_table.set("in_bytes", record_vec[14].clone())?;
-                                                                                   lua_table.set("out_bytes", record_vec[15].clone())?;
-                                                                                   lua_table.set("fin_cnt", record_vec[16].clone())?;
-                                                                                   lua_table.set("syn_cnt", record_vec[17].clone())?;
-                                                                                   lua_table.set("rst_cnt", record_vec[18].clone())?;
-                                                                                   lua_table.set("psh_cnt", record_vec[19].clone())?;
-                                                                                   lua_table.set("ack_cnt", record_vec[20].clone())?;
-                                                                                   lua_table.set("urg_cnt", record_vec[21].clone())?;
-                                                                                   lua_table.set("ece_cnt", record_vec[22].clone())?;
-                                                                                   lua_table.set("cwr_cnt", record_vec[23].clone())?;
-                                                                                   lua_table.set("ns_cnt", record_vec[24].clone())?;
-                                                                                   lua_table.set("prot", record_vec[25].clone())?;
-                                                                                   lua_table.set("tos", record_vec[26].clone())?;
-
-                                                                                   // Assuming there's a function in Lua named `process_data`
-                                                                                   for plugin_name in plugins.iter() {
-                                                                                   let plugin_table: rlua::Table = ctx.globals().get(plugin_name.as_str())?;
-                                                                                   let func: rlua::Function = plugin_table.get("process_data")?;
-                                                                                   func.call(lua_table.clone())?;
-                                                                                   }
-                                                                                   Ok(())
-                                                                                   });
-                                                                                   }
-                                                                                   })));
-                                                                                    */
+                                                                             
         Ok(PluginManager {
             lua,
             sender,
