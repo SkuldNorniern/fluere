eabf3594367861ca34f6e6804df659a9980e2152
SkuldNorniern <skuldnorniern@gmail.com>
2023-09-06 15:47:15 +0900

    style: reformat recent commit

--- fluere-plugin/src/util.rs
+++ fluere-plugin/src/util.rs
@@ -22,7 +22,7 @@ pub fn home_cache_path() -> PathBuf {
 
     let path_config = path_base.join("fluere");
     if !path_config.exists() {
-            fs::create_dir_all(path_config.clone()).unwrap();
-        }
+        fs::create_dir_all(path_config.clone()).unwrap();
+    }
     path_config
 }
