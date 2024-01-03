3b22f159293699e42a1389d939ceee18145aa11d
SkuldNorniern <SkuldNorniern@users.noreply.github.com>
2024-01-03 00:45:10 +0000

    style: `rustfmt`

--- fluere-plugin/src/downloader.rs
+++ fluere-plugin/src/downloader.rs
@@ -14,7 +14,6 @@ pub fn download_plugin_from_github(repo_name: &str) -> Result<(), std::io::Error
     let repo_path = path.join(repo_name.split('/').last().unwrap());
     let repository_path = Path::new(&repo_path);
     // println!("repository_path: {:?}", repository_path);
-   
 
     match git2::Repository::open(repository_path) {
         Ok(repo) => {
