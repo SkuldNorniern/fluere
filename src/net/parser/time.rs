ce6903f786d78948c53d637ba47ba8ec83ccd92b
SkuldNorniern <skuldnorniern@gmail.com>
2024-03-12 10:25:41 +0900

    fix: remove deprecated function

--- src/net/parser/time.rs
+++ src/net/parser/time.rs
@@ -1,14 +1,17 @@
 use chrono::prelude::*;
+use chrono::DateTime;
 
+#[inline]
 pub fn parse_microseconds(sec: u64, usec: u64) -> u64 {
     sec * 1000000 + usec
 }
 
+#[inline]
 pub fn microseconds_to_timestamp(usec: u64) -> String {
-    let naive = NaiveDateTime::from_timestamp_opt(usec as i64, 0);
-
+    let naive = DateTime::from_timestamp(usec as i64, 0).unwrap().naive_utc();
+    
     #[cfg(not(target_os = "windows"))]
-    let datetime = DateTime::<Utc>::from_naive_utc_and_offset(naive.unwrap(), Utc);
+    let datetime = DateTime::<Utc>::from_naive_utc_and_offset(naive, Utc);
 
     #[cfg(target_os = "windows")]
     let datetime = DateTime::<Utc>::from_utc(naive.unwrap(), Utc);
