6f70f7339775c9aab7ecbf27008ab5fe15062227
SkuldNorniern <SkuldNorniern@users.noreply.github.com>
2024-03-12 05:01:48 +0000

    style: `rustfmt`

--- src/net/parser/time.rs
+++ src/net/parser/time.rs
@@ -8,8 +8,10 @@ pub fn parse_microseconds(sec: u64, usec: u64) -> u64 {
 
 #[inline]
 pub fn microseconds_to_timestamp(usec: u64) -> String {
-    let naive = DateTime::from_timestamp(usec as i64, 0).unwrap().naive_utc();
-    
+    let naive = DateTime::from_timestamp(usec as i64, 0)
+        .unwrap()
+        .naive_utc();
+
     #[cfg(not(target_os = "windows"))]
     let datetime = DateTime::<Utc>::from_naive_utc_and_offset(naive, Utc);
 
