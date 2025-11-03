b8e6028e77210145785bfeb8fb061d058d52cee5
SkuldNorniern <skuldnorniern@gmail.com>
2025-11-03 15:12:21 +0900

    refactor: `rustclippy`

--- src/logger.rs
+++ src/logger.rs
@@ -79,7 +79,7 @@ impl Log for Logger {
     }
 
     fn log(&self, record: &Record) {
-        let timestamp = Local::now().format("%Y-%m-%d %H:%M:%S %z").to_string();
+        let _timestamp = Local::now().format("%Y-%m-%d %H:%M:%S %z").to_string();
         let formatted_message = format!(
             "[{}] [{}:{}]: {}",
             record.level(),
