ba9577fa76a2dac150df61a86d8aa2f2a42b1993
Eira <eira@nornity.com>
2026-07-17 12:25:33 +0000

    refactor: remove pnet dependency entirely

--- src/net/parser/mod.rs
+++ src/net/parser/mod.rs
@@ -1,15 +1,11 @@
-mod flags;
 mod fluereflows;
 mod keys;
-mod ports;
 mod raw;
 mod time;
 mod tos;
 
-pub use flags::parse_flags;
 pub use fluereflows::parse_fluereflow;
 pub use keys::parse_keys;
-pub use ports::parse_ports;
 pub use time::microseconds_to_timestamp;
 pub use time::parse_microseconds;
 pub use tos::dscp_to_tos;
