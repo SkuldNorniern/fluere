0c863d514b8b43251a47b451c6570cba5b31f946
Eira <eira@nornity.com>
2026-07-17 09:05:32 +0000

    refactor: propagate errors in cli, capture, pcap mode

--- src/net/packet_pcap.rs
+++ src/net/packet_pcap.rs
@@ -1,54 +1,45 @@
 use std::fs;
 use std::time::{Duration, Instant};
 
+use crate::error::{FluereError, OptionExt};
 use crate::net::{CaptureDevice, find_device};
 use crate::types::Args;
 use crate::utils::cur_time_file;
 
 use log::{debug, trace};
 
-pub async fn pcap_capture(args: Args) {
-    let pcap_file = args.files.pcap.unwrap();
-    let interface_name = args.interface.expect("interface not found");
-    let duration = args.parameters.duration.unwrap();
-    let _interval = args.parameters.interval.unwrap();
-    let _sleep_windows = args.parameters.sleep_windows.unwrap();
+pub async fn pcap_capture(args: Args) -> Result<(), FluereError> {
+    let pcap_file = args.files.pcap.required("pcap")?;
+    let interface_name = args.interface.required("interface")?;
+    let duration = args.parameters.duration.required("duration")?;
+    let _interval = args.parameters.interval.required("interval")?;
+    let _sleep_windows = args.parameters.sleep_windows.required("sleep_windows")?;
 
-    let interface = find_device(interface_name.as_str()).unwrap();
-    let mut cap_device = CaptureDevice::new(interface.clone()).unwrap();
+    let interface = find_device(interface_name.as_str())?;
+    let mut cap_device = CaptureDevice::new(interface)?;
     let cap = &mut cap_device.capture;
 
     let file_dir = "./output";
     // let mut packet_count = 0;
-    match fs::create_dir_all(<&str>::clone(&file_dir)) {
-        Ok(_) => debug!("Created directory: {}", file_dir),
-        Err(error) => panic!("Problem creating directory: {:?}", error),
-    };
+    fs::create_dir_all(file_dir)?;
+    debug!("Created directory: {}", file_dir);
 
     let file_path = cur_time_file(pcap_file.as_str(), file_dir, ".pcap");
-    let mut file: pcap::Savefile = match cap.savefile(file_path.as_ref()) {
-        Ok(f) => f,
-        Err(_) => std::process::exit(1),
-    };
+    let mut file: pcap::Savefile = cap.savefile(file_path.as_ref())?;
 
     let start = Instant::now();
 
     loop {
-        match cap.next_packet() {
-            Err(_) => {
-                continue;
-            }
-            Ok(packet) => {
-                trace!("received packet");
-                //println!("packet: {:?}", packet);
-                file.write(&packet);
+        let packet = cap.next_packet()?;
+        trace!("received packet");
+        //println!("packet: {:?}", packet);
+        file.write(&packet);
 
-                // Check if the duration has been reached
-                if start.elapsed() >= Duration::from_millis(duration) && duration != 0 {
-                    break;
-                }
-            }
+        // Check if the duration has been reached
+        if start.elapsed() >= Duration::from_millis(duration) && duration != 0 {
+            break;
         }
     }
     debug!("Captured in {:?}", start.elapsed());
+    Ok(())
 }
